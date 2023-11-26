876813557c10c0c34fb0b7349bfb62412910ed97
mrzslz <moritz.schulz@proton.me>
2023-11-26 11:15:56 +0100

    performance test real aspa routes

--- tests/test_performance_aspa.c
+++ tests/test_performance_aspa.c
@@ -22,12 +22,12 @@ static char *buffer_file(char *fname) {
     long fsize = ftell(f);
     fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
 
-    char *string = lrtr_malloc(fsize);
+    char *string = lrtr_malloc(fsize+4);
     fread(string, fsize, 1, f);
+    *(uint32_t*)(&string[fsize]) = 0;
     fclose(f);
     return string;
 }
-
 
 enum as_path_verification_result *verify_as_path(struct aspa_table *aspa_table, struct rtr_socket* rtr_socket, char* f, size_t batch_size){
     uint32_t nwalks = *(uint32_t*)(&f[0]);
@@ -46,6 +46,74 @@ enum as_path_verification_result *verify_as_path(struct aspa_table *aspa_table, 
     }
 
     return results;
+}
+
+bool verify_as_path2(struct aspa_table *aspa_table, char* f)
+{
+    uint32_t plen = *(uint32_t*)f;
+    size_t pos = sizeof(uint32_t);
+
+    int count = 0;
+
+    do {
+        enum as_path_verification_result result = as_path_verify_upstream(aspa_table, &f[pos], plen);
+        if (result != AS_PATH_VALID) {
+            printf("%d %d %lu ", pos, plen, *(uint32_t*)(&f[pos]));
+            for (uint32_t j = 0; j < plen; j++) {
+                printf("%d -> ", *(uint32_t*)(&f[pos + j*sizeof(uint32_t)]));
+            }
+            printf("\n");
+            return false;
+        }
+        pos += plen*sizeof(uint32_t);
+
+        plen = *(uint32_t*)(&f[pos]);
+        pos += sizeof(uint32_t);
+        count++;
+    } while (plen != 0);
+
+    printf("%d ", count);
+
+    return true;
+}
+
+bool verify_as_path3(struct aspa_table *aspa_table, char* fname)
+{
+    FILE *f = fopen(fname, "rb");
+    assert(f != NULL);
+
+    uint32_t plen;
+
+    size_t readc = fread(&plen, sizeof(uint32_t), 1, f);
+    assert(readc == 1);
+
+    int count = 0;
+
+    uint32_t* buffer = NULL;
+
+    do {
+            buffer = (uint32_t*)realloc((size_t)(plen+1)*sizeof(uint32_t));
+            readc = fread((char*)buffer, sizeof(uint32_t), plen+1, f);
+            assert(readc == (plen+1) || readc == plen);
+
+            enum as_path_verification_result result = as_path_verify_upstream(aspa_table, buffer, plen);
+            if (result != AS_PATH_VALID) {
+                printf("%d ", plen);
+                for (uint32_t j = 0; j < plen; j++) {
+                    printf("%d -> ", buffer[j]);
+                }
+                printf("\n");
+                return false;
+            }
+
+            plen = readc == plen ? 0 : buffer[plen];
+
+        count++;
+    } while (plen != 0);
+
+    printf("%d ", count);
+
+    return true;
 }
 
 static struct aspa_table *load_aspa_table(struct aspa_table* aspa_table, struct rtr_socket* rtr_socket, char *f) {
@@ -97,37 +165,27 @@ void run()
     #define D(b) ((double)(c[b]-c[b-1])/CLOCKS_PER_SEC)
 
     c[0] = clock();
-    char *f = buffer_file("/home/moritz/code/oss/rtrlib/algo/aspa.dump");
+    char *f = buffer_file("/home/moritz/code/oss/rtrlib/algo/dumps/aspa2.dump");
     c[1] = clock();
 	load_aspa_table(aspa_table, rtr_socket, f);
     c[2] = clock();
 
     printf("buffer %f parse %f ", D(1), D(2));
 
-    char *w = buffer_file("/home/moritz/code/oss/rtrlib/algo/rwalks.dump");
+    char *w = buffer_file("/home/moritz/code/oss/rtrlib/algo/dumps/routes.dump");
     c[3] = clock();
 
-    verify_as_path(aspa_table, rtr_socket, w, 10);
+    assert(verify_as_path2(aspa_table, w) == 1);
     c[4] = clock();
-    verify_as_path(aspa_table, rtr_socket, w, 100);
-    c[5] = clock();
-    verify_as_path(aspa_table, rtr_socket, w, 1000);
-    c[6] = clock();
-    verify_as_path(aspa_table, rtr_socket, w, 2500);
-    c[7] = clock();
-    verify_as_path(aspa_table, rtr_socket, w, 7500);
-    c[8] = clock();
 
 
-    printf("buffer %f verify 10: %f 100: %f 1000: %f 2500: %f 7500: %f\n",
-        D(3), D(4), D(5), D(6), D(7), D(8));
+    printf("buffer %f verify %f\n",
+        D(3), D(4));
 
 }
 
 int main() {
-    for (int i = 0; i < 20; i++) {
-        run();
-    }
+    run();
     return 0;
 }
 
