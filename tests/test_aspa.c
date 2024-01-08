7a1ed99972c52e2d29bc07acafd9eac48898afaa
mrzslz <moritz.schulz@proton.me>
2024-01-09 00:37:53 +0100

    add aspa test scenarios

--- tests/test_aspa.c
+++ tests/test_aspa.c
@@ -55,29 +55,31 @@ static int custom_recv(const struct tr_socket *socket, const void *buf, const si
 	return rlen;
 }
 
-static void begin_cache_response(uint8_t version, uint16_t session_id)
+static struct pdu_cache_response *begin_cache_response(uint8_t version, uint16_t session_id)
 {
-	if (data) {
-		lrtr_free(data);
-		data = NULL;
-		data_size = 0;
-		data_index = 0;
-	}
+	if (data == NULL)
+		data = lrtr_malloc(sizeof(struct pdu_cache_response));
+	else
+		data = lrtr_realloc(data, data_size + sizeof(struct pdu_cache_response));
 
-	data = lrtr_malloc(sizeof(struct pdu_cache_response));
 	assert(data);
 
-	struct pdu_cache_response *cache_response = (struct pdu_cache_response *)data;
+	struct pdu_cache_response *cache_response = (struct pdu_cache_response *)(data + data_size);
 	cache_response->ver = version;
 	cache_response->type = CACHE_RESPONSE;
 	cache_response->session_id = c16(session_id);
 	cache_response->len = c32(8);
 
 	data_size += sizeof(struct pdu_cache_response);
+
+	return cache_response;
 }
 
-static void append_aspa(uint8_t version, uint8_t flags, uint32_t customer_asn, uint32_t provider_asns[],
-			size_t provider_count)
+static uint8_t ASPA_ANNOUNCE = 1;
+static uint8_t ASPA_WITHDRAW = 0;
+
+static struct pdu_aspa *append_aspa(uint8_t version, uint8_t flags, uint32_t customer_asn, uint32_t provider_asns[],
+				    size_t provider_count)
 {
 	size_t pdu_size = sizeof(struct pdu_aspa) + sizeof(uint32_t) * provider_count;
 
@@ -100,9 +102,11 @@ static void append_aspa(uint8_t version, uint8_t flags, uint32_t customer_asn, u
 
 	memcpy(aspa->provider_asns, provider_asns, sizeof(uint32_t) * provider_count);
 	data_size += pdu_size;
+
+	return aspa;
 }
 
-static void end_cache_response(uint8_t version, uint16_t session_id, uint32_t sn)
+static struct pdu_end_of_data_v1_v2 *end_cache_response(uint8_t version, uint16_t session_id, uint32_t sn)
 {
 	data = lrtr_realloc(data, data_size + sizeof(struct pdu_end_of_data_v1_v2));
 	assert(data);
@@ -113,11 +117,13 @@ static void end_cache_response(uint8_t version, uint16_t session_id, uint32_t sn
 	eod->session_id = c16(session_id);
 	eod->len = c32(24);
 	eod->sn = c32(sn);
-	eod->refresh_interval = 0;
-	eod->retry_interval = 0;
-	eod->expire_interval = 0;
+	eod->refresh_interval = c32(RTR_REFRESH_MIN);
+	eod->retry_interval = c32(RTR_RETRY_MIN);
+	eod->expire_interval = c32(RTR_EXPIRATION_MIN);
 
 	data_size += sizeof(struct pdu_end_of_data_v1_v2);
+
+	return eod;
 }
 
 static void test_table(struct rtr_socket *socket, struct aspa_record records[], size_t record_count)
@@ -148,13 +154,13 @@ static void test_table(struct rtr_socket *socket, struct aspa_record records[], 
 	}
 }
 
-static void test_sync(struct rtr_socket *socket)
+static void test_regular_announcement(struct rtr_socket *socket)
 {
 	// Test: regular announcement
 	// Expect: OK
 	// DB: inserted
 	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
-	append_aspa(RTR_PROTOCOL_VERSION_2, 1, 1100, (uint32_t[]){1101, 1102, 1103, 1104}, 4);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 1100, (uint32_t[]){1101, 1102, 1103, 1104}, 4);
 	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 437);
 	assert(rtr_sync(socket) == RTR_SUCCESS);
 	test_table(socket,
@@ -162,13 +168,19 @@ static void test_sync(struct rtr_socket *socket)
 							       .provider_count = 4,
 							       .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}}},
 		   1);
-	printf("TEST: regular announcement: as expected\n\n");
+}
 
-	// Test: Announce existing record
+static void test_announce_existing(struct rtr_socket *socket)
+{
+	// Test: Announce for existing customer_asn
 	// Expect: ERROR
 	// DB: no change
+
+	// announces 1100 -> 1101, 1102, 1103, 1104
+	test_regular_announcement(socket);
+
 	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
-	append_aspa(RTR_PROTOCOL_VERSION_2, 1, 1100, (uint32_t[]){201, 2202, 2203, 2204}, 4);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 1100, (uint32_t[]){2201, 2202, 2203, 2204}, 4);
 	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
 	assert(rtr_sync(socket) == RTR_ERROR);
 	test_table(socket,
@@ -176,53 +188,231 @@ static void test_sync(struct rtr_socket *socket)
 							       .provider_count = 4,
 							       .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}}},
 		   1);
-	printf("TEST: announce existing: as expected\n\n");
+}
 
-	// Test: Announce new record twice (duplicate)
+static void test_announce_twice(struct rtr_socket *socket)
+{
+	// Test: Announce new record again (duplicate)
 	// Expect: ERROR
 	// DB: no change
 
-	// Test: Withdraw non-existant record
+	// announces 1100 -> 1101, 1102, 1103, 1104
+	test_regular_announcement(socket);
+
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 1100, (uint32_t[]){1101, 1102, 1103, 1104}, 4);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_ERROR);
+	test_table(socket,
+		   (struct aspa_record[]){(struct aspa_record){.customer_asn = 1100,
+							       .provider_count = 4,
+							       .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}}},
+		   1);
+}
+
+static void test_withdraw_nonexisting(struct rtr_socket *socket)
+{
+	// Test: Withdraw non-existent record
 	// Expect: ERROR
 	// DB: no change
 
+	// announces 1100 -> 1101, 1102, 1103, 1104
+	test_regular_announcement(socket);
+
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_WITHDRAW, 3300, (uint32_t[]){}, 0);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_ERROR);
+	test_table(socket,
+		   (struct aspa_record[]){(struct aspa_record){.customer_asn = 1100,
+							       .provider_count = 4,
+							       .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}}},
+		   1);
+}
+
+static void test_announce_withdraw(struct rtr_socket *socket)
+{
 	// Test: Announce record, immediately withdraw within same sync op
 	// Expect: OK
 	// DB: no change
 
+	// announces 1100 -> 1101, 1102, 1103, 1104
+	test_regular_announcement(socket);
+
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 3300, (uint32_t[]){3301, 3302, 3303, 3304}, 4);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_WITHDRAW, 3300, (uint32_t[]){}, 0);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_SUCCESS);
+	test_table(socket,
+		   (struct aspa_record[]){(struct aspa_record){.customer_asn = 1100,
+							       .provider_count = 4,
+							       .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}}},
+		   1);
+}
+
+static void test_withdraw_announce(struct rtr_socket *socket)
+{
 	// Test: Withdraw existing record, immediately announce record with same ASN within same sync op
 	// Expect: OK
 	// DB: record gets replaced
-    
-    // Test: regular announcements and withdrawals
-    // Expect: OK
-    // DB: records get removed, newly announced are added
+
+	// announces 1100 -> 1101, 1102, 1103, 1104
+	test_regular_announcement(socket);
+
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_WITHDRAW, 1100, (uint32_t[]){}, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 1100, (uint32_t[]){2201, 2202, 2203, 2204}, 4);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_SUCCESS);
+	test_table(socket,
+		   (struct aspa_record[]){(struct aspa_record){.customer_asn = 1100,
+							       .provider_count = 4,
+							       .provider_asns = (uint32_t[]){2201, 2202, 2203, 2204}}},
+		   1);
 }
 
-int main()
+static void test_regular(struct rtr_socket *socket)
 {
-	struct tr_socket *tr_socket = lrtr_malloc(sizeof(struct tr_socket));
-	tr_socket->recv_fp = (tr_recv_fp)&custom_recv;
-	tr_socket->send_fp = (tr_send_fp)&custom_send;
+	// Test: regular announcements and withdrawals
+	// Expect: OK
+	// DB: records get removed, newly announced are added
 
-	struct rtr_socket *socket = lrtr_malloc(sizeof(struct rtr_socket));
-	assert(socket);
-	socket->version = 2;
-	socket->state = RTR_SYNC;
-	socket->tr_socket = tr_socket;
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 1100, (uint32_t[]){1101, 1102, 1103, 1104}, 4);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 2200, (uint32_t[]){2201, 2202, 2203, 2204}, 4);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 437);
+	assert(rtr_sync(socket) == RTR_SUCCESS);
+	test_table(socket,
+		   (struct aspa_record[]){
+			   (struct aspa_record){.customer_asn = 1100,
+						.provider_count = 4,
+						.provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}},
+			   (struct aspa_record){.customer_asn = 2200,
+						.provider_count = 4,
+						.provider_asns = (uint32_t[]){2201, 2202, 2203, 2204}},
+		   },
+		   2);
 
-	struct aspa_table *aspa_table = lrtr_malloc(sizeof(*aspa_table));
-	assert(aspa_table);
-	aspa_table_init(aspa_table, NULL);
-	socket->aspa_table = aspa_table;
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_WITHDRAW, 2200, (uint32_t[]){}, 0);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 3300, (uint32_t[]){3301, 3302, 3303, 3304}, 4);
+	append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 0, (uint32_t[]){}, 0);
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_SUCCESS);
+	test_table(
+		socket,
+		(struct aspa_record[]){
+			(struct aspa_record){.customer_asn = 0, .provider_count = 0, .provider_asns = (uint32_t[]){}},
+			(struct aspa_record){.customer_asn = 1100,
+					     .provider_count = 4,
+					     .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}},
+			(struct aspa_record){.customer_asn = 3300,
+					     .provider_count = 4,
+					     .provider_asns = (uint32_t[]){3301, 3302, 3303, 3304}},
+		},
+		3);
+}
 
-	test_sync(socket);
+static void test_corrupt(struct rtr_socket *socket)
+{
+	// Test: send corrupt pdu after having received valid data
+	// Expect: Error
+	// DB: DB stays the same
 
+	test_regular(socket);
+
+	begin_cache_response(RTR_PROTOCOL_VERSION_2, 0);
+	struct pdu_aspa *aspa =
+		append_aspa(RTR_PROTOCOL_VERSION_2, ASPA_ANNOUNCE, 4400, (uint32_t[]){4401, 4402, 4403, 4404}, 4);
+
+	// corrupt ASPA len
+	aspa->len = c32(c32(aspa->len) + 1);
+
+	end_cache_response(RTR_PROTOCOL_VERSION_2, 0, 444);
+	assert(rtr_sync(socket) == RTR_ERROR);
+	test_table(
+		socket,
+		(struct aspa_record[]){
+			(struct aspa_record){.customer_asn = 0, .provider_count = 0, .provider_asns = (uint32_t[]){}},
+			(struct aspa_record){.customer_asn = 1100,
+					     .provider_count = 4,
+					     .provider_asns = (uint32_t[]){1101, 1102, 1103, 1104}},
+			(struct aspa_record){.customer_asn = 3300,
+					     .provider_count = 4,
+					     .provider_asns = (uint32_t[]){3301, 3302, 3303, 3304}},
+		},
+		3);
+}
+
+static void reset_data()
+{
+	printf("resetting...\n");
 	if (data) {
 		lrtr_free(data);
 		data = NULL;
 		data_size = 0;
 		data_index = 0;
 	}
+}
+
+static struct rtr_socket *fresh_socket()
+{
+	struct tr_socket *tr_socket = lrtr_calloc(1, sizeof(struct tr_socket));
+	tr_socket->recv_fp = (tr_recv_fp)&custom_recv;
+	tr_socket->send_fp = (tr_send_fp)&custom_send;
+
+	struct rtr_socket *socket = lrtr_calloc(1, sizeof(struct rtr_socket));
+	assert(socket);
+	socket->version = 2;
+	socket->state = RTR_SYNC;
+	socket->tr_socket = tr_socket;
+
+	struct aspa_table *aspa_table = lrtr_calloc(1, sizeof(*aspa_table));
+	assert(aspa_table);
+	aspa_table_init(aspa_table, NULL);
+	socket->aspa_table = aspa_table;
+
+	return socket;
+}
+
+int main()
+{
+	reset_data();
+	printf("\n\ntest_regular_announcement\n\n");
+	test_regular_announcement(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_regular_announcement\n\n");
+	test_regular_announcement(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_announce_existing\n\n");
+	test_announce_existing(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_announce_twice\n\n");
+	test_announce_twice(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_withdraw_nonexisting\n\n");
+	test_withdraw_nonexisting(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_announce_withdraw\n\n");
+	test_announce_withdraw(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_withdraw_announce\n\n");
+	test_withdraw_announce(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_regular\n\n");
+	test_regular(fresh_socket());
+
+	reset_data();
+	printf("\n\ntest_corrupt\n\n");
+	test_corrupt(fresh_socket());
+
 	return 0;
 }
