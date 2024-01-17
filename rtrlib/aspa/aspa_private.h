cab58c9b3cf2d5bf3cfd3e63939991d9f24c8e4b
carl <115627588+carl-tud@users.noreply.github.com>
2024-01-17 15:50:06 +0100

    Add docs

--- rtrlib/aspa/aspa_private.h
+++ rtrlib/aspa/aspa_private.h
@@ -13,73 +13,76 @@
  * @brief The aspa_table is an abstract data structure to organize the validated Autonomous System Provider
  * Authorization  data received from an RPKI-RTR cache server.
  *
- * # Swap-In Update Mechanism
- * The ASPA table implements aggregated updating using an array of 'add record' and 'remove record' operations --
- * reducing iterations and memory allocations. E.g., these operations can be derived from a RTR cache response
- * containing ASPA PDUs. The ASPA table's **Swap-In** update mechanism both avoids blocking callers who want to
+ * # Updating an ASPA table
+ * ASPA tables implement aggregated updating using an array of 'add record' and 'remove record' operations --
+ * reducing iterations and memory allocations.  E.g., these operations can be derived from a RTR cache response.
+ * Currently, two distinct update mechanisms are supported: **Swap-In** and **In-Place** updates. Use
+ * `ASPA_UPDATE_IN_PLACE` (define if **In-Place**) to use toggle which implementation is used in the RTR module.
+ * The array of operations is effectively a diff to the table's previous state. This diff can be conveniently used to notify callers
+ * about changes once the update is applied.
+ *
+ * ## Swap-In Update Mechanism
+ * The ASPA table's **Swap-In** update mechanism avoids blocking callers who want to
  * verify an `AS_PATH` (and therefore need read access to the table) while an update is in progress and removes the
  * need for an *undo mechanism* in case the update to the ASPA table itself or some other action performed inbetween fails.
  *
+ * Performing an update using this mechanism involves these steps:
  * - **Compute Update**:
  *   Every time you want to update a given ASPA table, call `aspa_table_compute_update`. This will create a new ASPA
- * array, appending both existing records and new records. Everything needed to update the table is stored in an update
- * structure.
- * - **Apply Update**:
+ *   array, appending both existing records and new records. Everything needed to update the table is stored in an update structure.
+ * - **Apply Update** (optional):
  *   You may, but do not need to, apply the update to the table using `aspa_table_apply_update`. This will swap in the
- * newly created ASPA array in the table and notify clients about changes made to records during the update.
- * - **Finish Update**:
+ *   newly created ASPA array in the table and notify clients about changes made to records during the update.
+ * - **Finish Update** (mandatory):
  *   After computing the update -- regardless of whether said computation failed -- you must perform a finishing step
- * using `aspa_table_finish_update`. This will deallocate provider arrays and other data created during the update
- * that's now unused.
+ *   using `aspa_table_finish_update`. This will deallocate provider arrays and other data created during the update
+ *   that's now unused.
  *
- * ## Thread Safety
  * The implementation guarantess no changes are made to the ASPA table between calling `aspa_table_compute_update`
  * and `aspa_table_finish_update`.
  *
- * ## Special Cases
- * `aspa_table_compute_update_internal` handles the complexity arising from multiple announcements and withdrawals
- * in a RTR cache response. There're various cases that need to be handled appropriately:
- *   1. **Announcement of Existing Record**:
- *     The caller attempts to add a record that's already present in the table (`ASPA_DUPLICATE_RECORD`).
- *   2. **Duplicate Announcement**:
- *     The caller attempts to add two or more records with the same customer ASN (`ASPA_DUPLICATE_RECORD`).
- *   3. **Removal of Unknown Record**:
- *     The caller attempts to remove a record from the table that doesn't exist (`ASPA_RECORD_NOT_FOUND`).
- *   4. **Duplicate Removal**:
- *     The caller attempts to remove a record twice or more (`ASPA_RECORD_NOT_FOUND`).
- *   5. **Complementary Announcement/Withdrawal**:
- *     The caller attempts to first add a record and then wants to remove the same record. This is equivalent to a
- * no-op. In that case, the table's clients won't be notified about these two complementary records as they're
- * effectively annihilating each other.
+ * ## In-Place Update Mechanism
+ * The ASPA table's **Swap-In** update mechanism involves in-place modifications to the array of records and an undo function
+ * that undoes changes made previously.
  *
- * ## Implementation
- * `aspa_table_compute_update` is responsible for finding the existing array and creating a new one.
- * `aspa_table_compute_update_internal` tackles the beforementioned cases by first sorting the array of 'add' and
+ * Performing an update using this mechanism involves these steps:
+ *  - **Update**:
+ *   Every time you want to update a given ASPA table, call `aspa_table_update`. This will modify the ASPA
+ *   array. If the update fails, `failed_operation` will be set to the operation where the error occuring.
+ * - **Undo Update** (optional):
+ *   You may, but do not need to, undo the update using `aspa_table_undo_update`. This will undo all operations up
+ *   to `failed_operation` or all operations.
+ * - **Clean Up**:
+ *   After computing the update you should go through a cleanup step using `aspa_table_update_cleanup`. This 
+ *   will deallocate provider arrays and other data created during the update that's now unused.
+ *
+ * ## Special Cases
+ * There're various cases that need to be handled appropriately by both implementations.
+ *   1. **Add existing record**:
+ *     The caller attempts to add a record that's already present in the table (`ASPA_DUPLICATE_RECORD`).
+ *   2. **Duplicate adds**:
+ *     The caller attempts to add two or more records with the same customer ASN (`ASPA_DUPLICATE_RECORD`).
+ *   3. **Removal of unknown record**:
+ *     The caller attempts to remove a record from the table that doesn't exist (`ASPA_RECORD_NOT_FOUND`).
+ *   4. **Duplicate removal**:
+ *     The caller attempts to remove a record twice or more (`ASPA_RECORD_NOT_FOUND`).
+ *   5. **Complementary add/remove**:
+ *     The caller attempts to first add a record and then wants to remove the same record. This is equivalent to a
+ *     no-op. `ASPA_NOTIFY_NO_OPS` (either defined or not) determines if clients are notified about these no-ops.
+ *
+ * ## Implementation Details
+ * Both update mechanism implementations tackle the beforementioned cases by first sorting the array of 'add' and
  * 'remove' operations by their customer ASN stably. That is, 'add' and 'remove' operations dealing with matching
  * customer ASNs will remain in the same order as they arrived. This makes checking for cases 2 - *Duplicate
  * Announcement* and 4 - *Duplicate Removal* easy as possible duplicates are neighbors in the operations array.
  * Ordering the operations also enables skipping annihilating operations as described in case 5 - *Complementary
  * Announcement/Withdrawal*.
- *
- * `aspa_table_compute_update_internal` is comprised of a loop iterating over operations and a nested loop that adds
- * records  from the existing ASPA array with an ASN smaller than the current operation's ASN to the new array.
- * - If the next record in the existing array and the current 'add' operation have a matching customer ASN,
+ * Both implementations are comprised of a loop iterating over operations and a nested loop that handles
+ * records from the existing ASPA array with an ASN smaller than the current operation's ASN.
+ * - If the record in the existing array and the current 'add' operation have a matching customer ASN,
  *   that's case 1 - *Announcement of Existing Record*.
- * - If the next record in the existing array and the current 'remove' operation do not have a matching customer ASN,
+ * - If the record in the existing array and the current 'remove' operation do not have a matching customer ASN,
  *   that's case 3 - *Removal of Unknown Record*.
- *
- * ## Change Notifications
- * The array of operations is effectively a diff to the table's previous state. This diff is used to notify callers
- * about changes once the update is applied.
- *
- * ## Cleanup Considerations
- * Each operation contains a record which in turn may hold a reference to an array of provider ASNs.
- * If the *update is not applied*, the provider arrays in each of the 'add' operations must be released.
- * After *applying the update*:
- *   1. **Removed Records**: Provider arrays of removed records must also be deallocated.
- *   2. **Complementary Announcements/Withdrawals**: The 'add' operation that's annihilated by its neighboring 'remove'
- * operation contains a provider array that's not included in the updated ASPA table. In turn, this provider array must
- * be deallocated.
  *
  * @{
  */
